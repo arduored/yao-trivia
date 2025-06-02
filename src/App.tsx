ae71897d466f9b6dfdd4952acad8c15c59342968
arduored <edouard.souan@zohomail.eu>
2025-06-02 12:07:36 +0200

    Feat: randomize correct answer position and add hearts for lifes

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import { Suspense, useEffect, useState } from 'react'
 import './App.css'
 import { fetchOneQuestion } from "./api/requests.ts"
 import { useOpenTDBToken } from "./hooks/useOpenTDBToken.ts"
+import HeartImg from "./assets/heart.svg"
 
 export default function App() {
 	const token = useOpenTDBToken() 
 	const [question, setQuestion] = useState<null|Question>(null);
+	const [answers, setAnswers] = useState([]);
 	const [selectedAnswer, setSelectedAnswer] = useState(null);
 	const [score, setScore] = useState(0);
 	const [lifes, setLifes] = useState(3);
 
 	useEffect(() => {
-		if(question) return
+		if(question) {
+			getAnswers();
+			return
+		}
 		(async() => {
 			const result = await fetchOneQuestion(token);
 
@@ -22,7 +27,16 @@ export default function App() {
 	}, [question])
 
 	function getAnswers() {
-		return [question.correct_answer, ...question.incorrect_answers]
+		const shuffledAnswers = [];
+		const answers = [question.correct_answer, ...question.incorrect_answers]
+
+		while (answers.length > 0) {
+			const idx = Math.floor(Math.random() * answers.length)
+			shuffledAnswers.push(answers[idx])
+			answers.splice(idx, 1)
+		}
+
+		setAnswers(shuffledAnswers)
 	}
 
 	function handleSelection(e) {
@@ -42,13 +56,15 @@ export default function App() {
 				alert("You lost!");
 			}
 		}
-		setQuestion(null);
+			setQuestion(null);
 	}
 
   return (<Suspense fallback={ <div>loading ... </div>}>
 	<div class="navbar">
 		<div id="score">{score}</div>
-		<div id="lifes">{lifes}</div>
+		<div id="lifes">
+			{Array(lifes).fill(HeartImg).map((heart, i) => <img key={i} src={heart} width={32}/>)}
+		</div>
 	</div>
 	<div className="card">
 		{question ? <div>
@@ -56,7 +72,7 @@ export default function App() {
 			<div dangerouslySetInnerHTML={{__html: `<h2>${question.question}</h2>`}} />
 			
 			 <div className="answers">
-				{getAnswers().map((i, idx) =>(<div key={idx} className="answer_group">
+				{answers.map((i, idx) =>(<div key={idx} className="answer_group">
 
 					<label htmlFor={`answer${i}`}>{i}</label>
 					<div>
